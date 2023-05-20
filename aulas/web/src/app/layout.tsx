b3634b46acaf539e410dc9e783b1e8c53aa987cd
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-05-20 18:24:32 -0300

    Cacei

--- aulas/web/src/app/layout.tsx
+++ aulas/web/src/app/layout.tsx
@@ -46,7 +46,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
             <Copyright />
           </div>
+          <div className='flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16'>
+
           {children}
+          </div>
         </main>
       </body>
     </html>
